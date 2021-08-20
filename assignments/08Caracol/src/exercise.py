beecc50be2335a3b65054d85ef4d0508127833a4
Daviddiaz22 <88851000+Daviddiaz22@users.noreply.github.com>
2021-08-19 23:57:29 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    minu= float(input('Dame los minutos: '))
+
+    recorrido = minu* 34.2
+
+    print('Centímentros recorridos: ' + str(recorrido))
 
 if __name__ == '__main__':
     main()
